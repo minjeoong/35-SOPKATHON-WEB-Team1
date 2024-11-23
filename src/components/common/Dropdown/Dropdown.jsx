da60553c65bdd46764297b0d58a9d2a85ee07c25
namdaeun <nde40345@gmail.com>
2024-11-24 00:07:05 +0900

    feat: 드롭다운 틀 구현

--- src/components/common/Dropdown/Dropdown.jsx
+++ src/components/common/Dropdown/Dropdown.jsx
@@ -1,9 +1,45 @@
-const Dropdown = () => {
+import { IcArrowDown, IcArrowUp } from '@/assets/svg';
+import * as S from '@/components/common/Dropdown/Dropdown.style';
+
+const Dropdown = ({
+	label,
+	options = [],
+	isOpen,
+	setIsOpen,
+	onTrigger,
+	selectedValue,
+	placeholder,
+	onSelect,
+}) => {
 	return (
-		<div>
-			<ul>
-				<li></li>
-			</ul>
+		<div css={S.wrapper}>
+			{label && (
+				<label id='label' css={S.label}>
+					{label}
+				</label>
+			)}
+			{selectedValue || placeholder}
+			<button onClick={onTrigger}>
+				{isOpen ? <IcArrowDown /> : <IcArrowUp />}
+			</button>
+			{isOpen && (
+				<ul css={S.itemList}>
+					{options.map((option, index) => (
+						<li
+							key={index}
+							value={option}
+							css={S.item}
+							tabIndex={0}
+							onClick={() => {
+								onSelect?.(option);
+								setIsOpen(false);
+							}}
+						>
+							{option}
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
---
+++ src/components/common/Dropdown/Dropdown.style.js
@@ -0,0 +1,58 @@
+import { theme } from '@/styles/theme';
+import { css } from '@emotion/react';
+
+export const wrapper = css`
+	position: relative;
+	display: flex;
+
+	justify-content: space-between;
+
+	gap: 4rem;
+
+	padding: 1rem 1.5rem;
+
+	border: 1.5px solid ${theme.colors.green};
+	border-radius: 4rem;
+
+	color: ${theme.colors.gray40};
+	background-color: ${theme.colors.gray10};
+
+	${theme.fonts.body2}
+`;
+
+export const label = css`
+	/* position: absolute; */
+
+	/* top: 4rem; */
+	${theme.fonts.title3}
+`;
+
+export const itemList = css`
+	position: absolute;
+
+	top: 4.5rem;
+
+	width: 32rem;
+	max-height: 15.2rem;
+
+	padding: 0.7rem 1.5rem;
+
+	justify-content: space-between;
+
+	border-radius: 8px;
+
+	background-color: ${theme.colors.gray10};
+
+	box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.1);
+	overflow-y: scroll;
+`;
+
+export const item = css`
+	display: block;
+
+	padding: 0.7rem 0;
+
+	border-bottom: 1px solid ${theme.colors.gray20};
+
+	${theme.fonts.body2}
+`;
