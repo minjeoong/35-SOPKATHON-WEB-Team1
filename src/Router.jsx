8d9f97de160d3aefa0191492f1c917b3e3980e0e
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2024-11-24 01:29:32 +0900

    feat: info페이지 추가, Header 컴포넌트 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage.jsx';
-
+import InfoPage from './pages/InfoPage/InfoPage.jsx'
 const router = createBrowserRouter([
 	{
-		path: '/',
-		element: <MainPage />,
+			path: '/',
+			element: <MainPage />,
+	},
+	{
+			path: '/info',
+			element: <InfoPage />,
 	},
 ]);
 
