a63d87dc8a8b46062385cdc715f51a50c0871826
namdaeun <nde40345@gmail.com>
2024-11-23 17:03:48 +0900

    fix: emotion css 수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+	plugins: [
+		react({
+			jsxImportSource: '@emotion/react',
+			babel: {
+				plugins: ['@emotion/babel-plugin'],
+			},
+		}),
+		svgr(),
+	],
 });
