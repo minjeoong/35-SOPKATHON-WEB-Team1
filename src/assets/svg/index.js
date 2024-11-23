188f6bb74c1c119a54024ce94366f937e5a3a016
youtheyeon <yuseoyeon@ewhain.net>
2024-11-24 00:34:53 +0900

    feat: info 페이지 input 컴포넌트 구현

--- src/assets/svg/index.js
+++
@@ -1,2 +0,0 @@
-export { default as IcCheckFlled } from './IcCheckFlled.jsx';
-export { default as IcCheckUnselect } from './IcCheckUnselect.jsx';
---
+++ src/assets/svg/index.jsx
@@ -0,0 +1,4 @@
+export { default as MoonAndStars } from './MoonAndStars';
+export { default as Sun } from './Sun';
+export { default as IcCheckFlled } from './IcCheckFlled';
+export { default as IcCheckUnselect } from './IcCheckUnselect';
