c181bacfa0a161e05fca29a1aa08f25ff6956192
namdaeun <nde40345@gmail.com>
2024-11-23 15:44:18 +0900

    feat: 기본 색상 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,12 @@
-const colors = {};
+const colors = {
+  black: "#161616",
+  white: "#ffffff",
+
+  gray40: "#626971",
+  gray30: "#c1c7cf",
+  gray20: "#e0e3e5",
+  gray10: "#eff1f2",
+};
 
 const fonts = {};
 
