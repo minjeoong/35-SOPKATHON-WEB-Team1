46d4e6d3340feb79a1fcd2b7cdbc2f32cd830eea
minjeoong <orange_mj@naver.com>
2024-11-23 23:43:33 +0900

    feat : Home 버튼 구현

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,18 @@
+import * as S from './MainPage.style';
+import Button from '@/components/common/Button/Button.jsx';
+
 const MainPage = () => {
-	return <div>MainPage</div>;
+	return (
+		<main css={S.MainWrap}>
+			<h1>메인 페이지</h1>
+			<p>메인 페이지입니다.</p>
+			<div css={S.ButtonWrap}>
+				<Button variant='primary' size='large'>
+					시작하기
+				</Button>
+			</div>
+		</main>
+	);
 };
 
 export default MainPage;
---
+++ src/pages/MainPage/MainPage.style.js
@@ -0,0 +1,14 @@
+import { css } from '@emotion/react';
+
+export const MainWrap = css`
+	background: linear-gradient(180deg, #8fff79 0%, #eff1f2 100%);
+	width: 100%;
+	height: 100vh;
+`;
+
+export const ButtonWrap = css`
+	text-align: center;
+	position: absolute;
+	bottom: 0;
+	margin: 3rem 2.7rem;
+`;
