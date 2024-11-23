fb131e4fe78c0043440fefc7cd7cff641e5372f3
namdaeun <nde40345@gmail.com>
2024-11-24 00:35:53 +0900

    chore: 테스트 코드 제거

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,35 +1,5 @@
-import Dropdown from '@/components/common/Dropdown/Dropdown';
-import { DATE } from '@/constants/date';
-import { wrapper } from '@/pages/MainPage/MainPage.style';
-import { useState } from 'react';
-
 const MainPage = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [selectedValue, setSelectedValue] = useState('');
-	const [isDisabled, setIsDisabled] = useState(true);
-
-	const handleTrigger = () => {
-		setIsOpen(!isOpen);
-	};
-
-	const handleSelect = (value) => {
-		setSelectedValue(value);
-	};
-
-	return (
-		<div css={wrapper}>
-			<Dropdown
-				onTrigger={handleTrigger}
-				isOpen={isOpen}
-				setIsOpen={setIsOpen}
-				options={DATE}
-				selectedValue={selectedValue}
-				onSelect={handleSelect}
-				placeholder='시간'
-				isDisabled={isDisabled}
-			/>
-		</div>
-	);
+	return <div></div>;
 };
 
 export default MainPage;
