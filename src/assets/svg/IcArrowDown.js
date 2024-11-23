9d4b3ef1020d27d2dc3798b44ac9d9256360e3ed
namdaeun <nde40345@gmail.com>
2024-11-24 00:07:30 +0900

    chore: 필요한 아이콘 가져옴

---
+++ src/assets/svg/IcArrowDown.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+const SvgIcArrowDown = (props) => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 16 16'
+		{...props}
+	>
+		<path
+			stroke='#626971'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			strokeWidth={2}
+			d='M13.333 5.333 8 10.667 2.667 5.333'
+		/>
+	</svg>
+);
+export default SvgIcArrowDown;
